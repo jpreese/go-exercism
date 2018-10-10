697902bc4d19f3d473de6671a83e4fbd4f61cf62
John Reese <jpreese@mtu.edu>
2018-10-10 14:04:34 -0400

    Initial commit

---
+++ collatz-conjecture/collatz_conjecture.go
@@ -0,0 +1,25 @@
+package collatzconjecture
+
+import "errors"
+
+func CollatzConjecture(input int) (int, error) {
+
+		steps := 0
+
+		if input <= 0 {
+				return 0, errors.New("input cannot be zero")
+		}
+
+		for input > 1 {
+				if input % 2 == 0 {
+						input = input / 2
+				} else {
+						input = (input * 3) + 1
+				}
+
+				steps++
+		}
+
+		return steps, nil
+}
+
